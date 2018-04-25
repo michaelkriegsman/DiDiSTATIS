6d90675db56142f2ecda146a9951c9d3dc6d85d3
Michael A. Kriegsman <kriegsman.1@gmail.com>
2018-04-25 13:21:59 -0500

    Cleaned MDS function

--- R/MDS.R
+++ R/MDS.R
@@ -4,23 +4,30 @@
 #'
 #'@param DATA The data
 #'@param data_are Flag to indicate data type #d, d2, CP
-#'@param DESIGN List of DESIGN info
+#'@param DESIGN_rows List of DESIGN info
 #'@param n2k Number of components to keep
-#'@param main Title for Factor Maps
-#'@return Factor maps, and a list of computational results
+#'@return Results of MDS
 #'@export
 
-MDS <- function(DATA, data_are=NULL, DESIGN, n2k=NULL, main = NULL){
+MDS <- function(DATA, data_are=NULL, DESIGN_rows, n2k=NULL){
 
   #Convert data to CP
   CP <- GetCP(DATA, data_are)
 
   #Eigen-decompose CP
-  res_MDS <- EigenCP(CP, DESIGN, n2k=n2k)
+  res_CP <- EigenCP_Full(CP = CP, DESIGN_rows = DESIGN_rows, n2k=n2k)
 
-  #Use that output to plot Factor Maps
-  PlotCP(res_CP = res_MDS, axes = c(1,2), main = main)
 
-  return(res_MDS)
+  #Build lists to organize output
+  input <- list()
+  input$DATA          <- DATA
+  input$data_are      <- data_are
+  input$CP            <- CP
+  input$DESIGN_rows   <- DESIGN_rows
+
+  returnME <- list(input = input,
+                   eig = res_CP$eig)
+
+  return(returnME)
 }
 
