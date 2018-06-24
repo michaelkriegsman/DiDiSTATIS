7302b7f4f026abe9c38289db7461dcbe0a1b51c5
Michael A. Kriegsman <kriegsman.1@gmail.com>
2018-06-24 17:13:06 -0500

    Brought in MusicFreeSort data

---
+++ R/data.R
@@ -0,0 +1,18 @@
+#data.R
+#'
+#' Free Sort of 36 Musical Pieces
+#'
+#' Data collected from the laboratory of Dr. W.J. Dowling
+#' The University of Texas at Dallas
+#'
+# #' @docType data
+#'
+# #' @usage data(MusicFreeSorting)
+#'
+#' @format The Music Free Sorting Data
+#' \describe{
+#'   \item{AlltheItems}{Sorting}
+#' }
+
+
+"MusicFreeSort"
